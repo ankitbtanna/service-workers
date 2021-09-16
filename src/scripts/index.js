565ac3d7bafc171521f1192b83fa449276a42a9e
Ankit Tanna <atanna@slb.com>
2021-09-16 18:15:08 +0530

    sw

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,29 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-if (process.env.NODE_ENV === 'development') {
-  require('../index.html');
+if (process.env.NODE_ENV === "development") {
+  require("../index.html");
 }
 
-console.log('webpack starterkit');
+console.log("webpack starterkit");
+
+setupSW();
+
+function setupSW() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registration) => {
+      console.log(registration);
+    });
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("sw.js", { scope: "/" }).then(
+        (registration) => {
+          console.log(
+            `Service Worker installed on scope ${registration.scope}`
+          );
+        },
+        (err) => {
+          console.log(`Service Worker failed to install ${err}`);
+        }
+      );
+    });
+  }
+}
